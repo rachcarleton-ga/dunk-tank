abbc517026f668add14942383e52e720a71eb946
Rachel Carleton <rachel.carleton@icloud.com>
2023-11-30 11:18:27 -0800

    Game HTML Update

--- main.js
+++ main.js
@@ -1,5 +1,56 @@
 /*----- constants -----*/
-
+const christmasNewYearWords = [
+    "Reindeer",
+    "Snowflake",
+    "Mistletoe",
+    "Stocking",
+    "Jingle",
+    "Caroling",
+    "Sleigh",
+    "Gingerbread",
+    "Tinsel",
+    "Nutcracker",
+    "Wreath",
+    "Chimney",
+    "Ornament",
+    "Fireplace",
+    "Frosty",
+    "Santa",
+    "Elf",
+    "Snowman",
+    "Holly",
+    "Poinsettia",
+    "Bells",
+    "Candlelight",
+    "Roasting",
+    "Garland",
+    "Snowball",
+    "Cocoa",
+    "Gifts",
+    "Jolly",
+    "Nativity",
+    "Star",
+    "Bethlehem",
+    "Countdown",
+    "Resolution",
+    "Midnight",
+    "Champagne",
+    "Party",
+    "Confetti",
+    "Family",
+    "Balloon",
+    "Countdown",
+    "Fireworks",
+    "January",
+    "Celebrate",
+    "Toast",
+    "Reflection",
+    "Presents",
+    "Sparkler",
+    "Festive",
+    "Cheers",
+    "Tradition"
+  ];
 
 	/*----- state variables -----*/
 
@@ -11,3 +62,10 @@
 
 
 	/*----- functions -----*/
+function initializeGame() {
+    renderboard();
+} 
+
+
+// Last line of code
+initializeGame();
