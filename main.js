cf3061869992797f2aaed27f7ed8b452383c1f19
Rachel Carleton <rachel.carleton@icloud.com>
2023-12-04 11:03:40 -0800

    updated format

--- main.js
+++ main.js
@@ -99,92 +99,84 @@ const holidayWords = [
   "toast",
   "snowdrift",
   "solstice",
-  "merry christmas!"
+  "merry christmas!",
 ];
 
-
-	/*----- state variables -----*/
+/*----- state variables -----*/
 
 let selectedWord;
 let guessedLetters;
 let numberOfGuesses = 0;
-	/*----- cached elements  -----*/
 
-
-	/*----- event listeners -----*/
-
-
-	/*----- functions -----*/
+/*----- functions -----*/
 function initializeGame() {
-  numberOfGuesses = 0
-  guessedLetters = []
-// select random word from the array 
-selectedWord = holidayWords[Math.floor(Math.random() * holidayWords.length)];
-
-console.log(selectedWord)
-//initialize array to store guessed letters 
-//guessedLetters = Array(selectedWord.length).fill('_');
-for (let letter of selectedWord) {
-  if (letter === ' ') {
-    guessedLetters.push('*')
-  } else {
-    guessedLetters.push('_')
+  numberOfGuesses = 0;
+  guessedLetters = [];
+  // select random word from the array
+  selectedWord = holidayWords[Math.floor(Math.random() * holidayWords.length)];
+  //initialize array to store guessed letters
+  for (let letter of selectedWord) {
+    if (letter === " ") {
+      guessedLetters.push("*");
+    } else {
+      guessedLetters.push("_");
+    }
   }
-  
-}
-// Display the initial state of the word
-updateWordDisplay();
-displayResult(" ");
+  // Display the initial state of the word
+  updateWordDisplay();
+  displayResult(" ");
 }
 
 function guessLetter(button) {
-    const guess = button.textContent.toLowerCase();
+  const guess = button.textContent.toLowerCase();
 
-    if (selectedWord.includes(guess)) {
-     // Update guessed letters and display
-      for (let i = 0; i < selectedWord.length; i++) {
-        if (selectedWord[i] === guess) {
-            guessedLetters[i] = guess;
-        }
+  if (selectedWord.includes(guess)) {
+    // Update guessed letters and display
+    for (let i = 0; i < selectedWord.length; i++) {
+      if (selectedWord[i] === guess) {
+        guessedLetters[i] = guess;
       }
-// Check if the entire word has been guessed
-    if (guessedLetters.join('') === selectedWord) {
-      displayResult('Congratulations! You stayed dry!');
+    }
+    // Check if the entire word has been guessed
+    if (guessedLetters.join("") === selectedWord) {
+      displayResult("Congratulations! You stayed dry!");
     }
   } else {
-    numberOfGuesses ++
-    console.log(numberOfGuesses) 
+    numberOfGuesses++;
     if (numberOfGuesses > 5) {
-      displayResult("You've been Dunked!")
+      displayResult("You've been Dunked!");
     } else {
-// Incorrect letter guessed
-displayResult(`Incorrect Guess. you have ${6 - numberOfGuesses} guesses left!`)
+      // Incorrect letter guessed
+      displayResult(
+        `Incorrect Guess. you have ${6 - numberOfGuesses} guesses left!`
+      );
     }
- // logic to track and display the dunk tank here
+    // logic to track and display the dunk tank here
   }
- // Update the word display
- updateWordDisplay();
+  // Update the word display
+  updateWordDisplay();
 }
 
- function updateWordDisplay() {
-  const wordDisplayElement = document.getElementById('word-display');
-  wordDisplayElement.textContent = guessedLetters.join(' ');
+function updateWordDisplay() {
+  const wordDisplayElement = document.getElementById("word-display");
+  wordDisplayElement.textContent = guessedLetters.join(" ");
 }
 
 function displayResult(message) {
-  const resultElement = document.getElementById('result');
+  const resultElement = document.getElementById("result");
   resultElement.textContent = message;
 }
 
-// event listeners for keyboard buttons 
-document.querySelectorAll('keyboard').forEach(button => {
-  button.addEventListener('click', function() {
-  guessLetter(this);
-});
+// event listeners for keyboard buttons
+document.querySelectorAll("keyboard").forEach((button) => {
+  button.addEventListener("click", function () {
+    guessLetter(this);
+  });
 });
 
-document.querySelector(".playAgain").addEventListener('click', function(){
-  initializeGame() 
-}) 
+// event listner for play again button
+document.querySelector(".playAgain").addEventListener("click", function () {
+  initializeGame();
+});
 // Last line of code
-initializeGame()
+initializeGame();
